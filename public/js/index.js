01fbf743d91e287a3af7112bb6a7a2567b7598be
Vera <vera.weikel@gmail.com>
2019-05-29 15:07:23 -0700

    Updates the files modal button deletion instance.

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+//console.log("index is working for showModal()"); TEST VIA C.LOG BEFORE CODING ANYTHING.
+function showModal() { //modal to grab modal e and hide by changing class of modal(e); remove to show/revive to hid
+
+}
+window.onload = function(){  //waiting for window fully to load prior to running any code for modal
+    const viewCommentButton = document.getElementsByClassName("view-comments");
+}
